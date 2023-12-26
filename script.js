dc1181af00328ebd73b1639bb9b49fb36ee85971
Rashu Kumar <rashukumar2000@gmail.com>
2023-12-26 11:52:03 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,30 @@
+let count=1;
+
+function incrementFunction() {
+   count++;
+   {
+    var a=10;
+    let b=20;
+    console.log(a)
+    console.log(b)
+   }
+   console.log("Inside Function"+ a);
+}
+incrementFunction()
+
+
+
+
+
+
+
+
+
+
+a=10
+
+var a;
+a=10
+b="10"
+
+a===b
